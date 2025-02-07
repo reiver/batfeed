1f2337d7f0256b93ca45802db11ae402446d3abd
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-07 01:44:04 -0800

    replacing logging with go-log

--- srv/db/db.go
+++ srv/db/db.go
@@ -7,35 +7,39 @@ import (
 	_ "modernc.org/sqlite"
 
 	"github.com/reiver/batfeed/cfg"
-	. "github.com/reiver/batfeed/srv/log"
+	"github.com/reiver/batfeed/srv/log"
 )
 
 var db *sql.DB
 
 func initDB(filename string) (*sql.DB, error) {
 
+	log := logsrv.Prefix("dbsrv")
+	log.Begin()
+	defer log.End()
+
 	var database *sql.DB
 	{
-		Logf("[dbsrv] opening database file %q ...", filename)
+		log.Informf("opening database file %q ...", filename)
 
 		var err error
 		database, err = sql.Open("sqlite", filename)
 		if nil != err {
-			Logf("[dbsrv] FAILURE — could not open database file %q: %w", filename, err)
+			log.Errorf("FAILURE — could not open database file %q: %w", filename, err)
 			return nil, erorr.Errorf("dbsrv: problem opening SQLite database file %q: %w", filename, err)
 		}
 		if nil == database {
-			Logf("[dbsrv] FAILURE — could not open database file %q: nil database", filename)
+			log.Errorf("FAILURE — could not open database file %q: nil database", filename)
 			return nil, erorr.Errorf("dbsrv: nil database wen opening SQLite database file %q", filename)
 		}
 
-		Logf("[dbsrv] success — opened database file %q", filename)
+		log.Informf("success — opened database file %q", filename)
 	}
 
 	{
 		err := initTables(database)
 		if nil != err {
-			Logf("[dbsrv] FAILURE — problem initializing tables in database file %q", filename)
+			log.Errorf("FAILURE — problem initializing tables in database file %q", filename)
 			return database, err
 		}
 	}
